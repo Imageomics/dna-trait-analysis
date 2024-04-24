8210cba42f9bb1d242d3e6da823fe4fc67297b73
David Carlyn <davidecarlyn@gmail.com>
2024-04-24 15:18:22 -0400

    moved files around and updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup, find_packages
+
 setup(
     name="dna-train-analysis",
-    version="1.0",
-    packages=find_packages(),
+    version="1.0.0",
+    url="https://github.com/Imageomics/dna-trait-analysis",
+    
+    author="",
+    author_email="",
+    
+    description="",
+    long_description="",
+    
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True
 )
