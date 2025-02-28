a72e93a3ceee15e89b8b5514a34f1125ea51bd13
David Carlyn <davidecarlyn@gmail.com>
2025-02-28 14:55:51 -0500

    Updated editing dashboard and some refactoring

--- src/gtp/dataloading/data_collectors.py
+++ src/gtp/dataloading/data_collectors.py
@@ -4,12 +4,17 @@ from gtp.dataloading.path_collectors import (
     get_post_processed_phenotype_directory,
     get_results_training_metadata_directory,
 )
-from gtp.dataloading.tools import load_chromosome_data, split_data_by_file
+from gtp.dataloading.tools import (
+    load_chromosome_data,
+    split_data_by_file,
+)
 from gtp.options.training import TrainingOptions
 from gtp.tools.timing import profile_exe_time
 
 
-def load_chromosome_and_phenotype_data(configs: GenotypeToPhenotypeConfigs, options: TrainingOptions):
+def load_chromosome_and_phenotype_data(
+    configs: GenotypeToPhenotypeConfigs, options: TrainingOptions
+):
     genotype_folder = get_post_processed_genotype_directory(configs.io)
     phenotype_folder = get_post_processed_phenotype_directory(configs.io)
 
@@ -27,12 +32,17 @@ def load_chromosome_and_phenotype_data(configs: GenotypeToPhenotypeConfigs, opti
     phenotype_data_aligned = phenotype_data_aligned[
         :, options.out_dims_start_idx : options.out_dims_start_idx + options.out_dims
     ]
-    
+
     return camids_aligned, genotype_data_aligned, phenotype_data_aligned
 
+
 @profile_exe_time(verbose=False)
-def load_training_data(configs: GenotypeToPhenotypeConfigs, options: TrainingOptions, return_camids=False):
-    camids_aligned, genotype_data_aligned, phenotype_data_aligned = load_chromosome_and_phenotype_data(configs, options)
+def load_training_data(
+    configs: GenotypeToPhenotypeConfigs, options: TrainingOptions, return_camids=False
+):
+    camids_aligned, genotype_data_aligned, phenotype_data_aligned = (
+        load_chromosome_and_phenotype_data(configs, options)
+    )
 
     metadata_folder = get_results_training_metadata_directory(configs.io)
     train_split, val_split, test_split = split_data_by_file(
@@ -42,7 +52,7 @@ def load_training_data(configs: GenotypeToPhenotypeConfigs, options: TrainingOpt
         metadata_folder,
         options.species,
     )
-    
+
     if return_camids:
         return train_split, val_split, test_split, camids_aligned
 
